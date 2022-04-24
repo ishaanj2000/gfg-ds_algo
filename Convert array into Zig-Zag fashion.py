f9d1b86b62eb16629697dc1634dc5b041502f493
ishaanj2000 <58269543+ishaanj2000@users.noreply.github.com>
2022-04-24 15:22:44 +0530

    Create Convert array into Zig-Zag fashion.py
    arrays

---
+++ Convert array into Zig-Zag fashion.py
@@ -0,0 +1,13 @@
+def zigZag(self,arr, n):
+		
+		flag = True # '<'
+		for i in range(n-1):
+		    if flag == True:
+    		    if arr[i] > arr[i+1]:
+    		        arr[i], arr[i+1] = arr[i+1], arr[i]
+		    else: # '>'
+		        if arr[i]<arr[i+1]:
+		            arr[i], arr[i+1] = arr[i+1], arr[i]
+		            
+		    flag = bool(1-flag)
+        return arr
