9286460e0c07524cc78efbd7ab94cfdf839ae5f6
ishaanj2000 <58269543+ishaanj2000@users.noreply.github.com>
2022-04-24 15:44:32 +0530

    Update Circular tour.py

--- Circular tour.py
+++ Circular tour.py
@@ -9,18 +9,18 @@ Note :  Assume for 1 litre petrol, the truck can go 1 unit of distance.'''
     lis[][1]:Distance
 '''
 
-def tour(self,lis, n):
+        def tour(self,lis, n):
         #Code here
-        trip_tank,cur_tank,start=0,0,0
-        
-        for i in range(n):
-            trip_tank+=(lis[i][0]-lis[i][1])
-            cur_tank+=(lis[i][0]-lis[i][1])
-            
-            if cur_tank<0:
-                cur_tank=0
-                start=i+1
-        
-        if trip_tank>=0:
-            return start
+            trip_tank,cur_tank,start=0,0,0
+
+            for i in range(n):
+                trip_tank+=(lis[i][0]-lis[i][1])
+                cur_tank+=(lis[i][0]-lis[i][1])
+
+                if cur_tank<0:
+                    cur_tank=0
+                    start=i+1
+
+            if trip_tank>=0:
+                return start
         return -1
